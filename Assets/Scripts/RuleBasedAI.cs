620fb01632dff91db2485a02fd8cb12a13cfc322
Markal03 <marco@nova-care.uk>
2020-12-21 14:41:28 +0000

    Implemented priority logic in RBS AI

--- Assets/Scripts/RuleBasedAI.cs
+++ Assets/Scripts/RuleBasedAI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using System.Linq;
+using System;
 
 public class RuleBasedAI : MonoBehaviour
 {
@@ -14,17 +15,29 @@ public class RuleBasedAI : MonoBehaviour
     private int destPoint = 0;
 
     public float minimumDistance;
+    public float minimumAllyDistance;
 
     private bool isEnemyNearby;
+    private bool isAllyNearby;
+
+    private int currentActionPriority;
 
     private Dictionary<float, GameObject> closeUnits = new Dictionary<float, GameObject>();
+    private Dictionary<float, GameObject> closeAllies = new Dictionary<float, GameObject>();
 
     private GameObject target;
+
+    private float talkCooldown = 300;
+    private float lastTalkTime = 300;
+
+    private bool isTalking = false;
     private void Awake()
     {
         objectInfo = gameObject.GetComponent<ObjectInfo>();
         agent = GetComponent<NavMeshAgent>();
         isEnemyNearby = false;
+        isAllyNearby = false;
+        currentActionPriority = 1;
     }
     void Start()
     {
@@ -39,35 +52,87 @@ public class RuleBasedAI : MonoBehaviour
 
     public void RunRuleset()
     {
-        SpotEnemy();
         Patrol();
+
+        SpotEnemy();
+
         Chase();
-        //Talk();
+
+        SpotAlly();
+        Talk();
+
+
     }
 
+    //Priority 1 - Low
     private void Patrol()
     {
-        if (!isEnemyNearby && !agent.pathPending && agent.remainingDistance < 1f)
+        if (!agent.pathPending && agent.remainingDistance < 1f && currentActionPriority == 1 && !isTalking)
         {
              GoToNextPoint();
         }
     }
 
+    //Priority 2 - Medium
+    private void Talk()
+    {
+        if (lastTalkTime < talkCooldown)
+        {
+            lastTalkTime += Time.deltaTime;
+            currentActionPriority = 1;
+
+        }
+        else if (currentActionPriority == 2)
+        {
+            target = GetClosestAlly();
+            agent.destination = target.transform.position;
+            Debug.Log("Starting to talk");
+            StartCoroutine(Wait(2, TalkCallback));
+            lastTalkTime = 0;
+        }
+    }
+
+    //Priority 3 - High
     private void Chase()
     {
-        if (isEnemyNearby)
+        if (currentActionPriority == 3)
         {
             target = GetClosestEnemy();
             agent.destination = target.transform.position;
+            gameObject.transform.LookAt(target.transform);
             objectInfo.SetAttackTarget(target);
         }
 
-
     }
+
+    private IEnumerator Wait(float duration, Action callback)
+    {
+        isTalking = true;
+        yield return new WaitForSeconds(duration);
+        callback();
+    }
+
+    void TalkCallback()
+    {
+        isTalking = false;
+        currentActionPriority = 1;
+    }
+    
     private void SpotEnemy()
     {
         isEnemyNearby = CheckIfTagIsNearby("Selectable");
+        this.currentActionPriority = isEnemyNearby ? 3 : 1;
     }
+    
+    private void SpotAlly()
+    {
+        isAllyNearby = CheckIfAllyTagIsNearby("Seeker");
+        if (currentActionPriority < 3)
+        { 
+            this.currentActionPriority = isAllyNearby ? 2 : 1;
+        }
+    }
+
     private bool CheckIfTagIsNearby(string _tag)
     {
         GameObject[] gameObjects = GameObject.FindGameObjectsWithTag(_tag);
@@ -84,6 +149,24 @@ public class RuleBasedAI : MonoBehaviour
         }
 
         return closeUnits.Count > 0;
+    }
+    
+    private bool CheckIfAllyTagIsNearby(string _tag)
+    {
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag(_tag);
+        closeAllies.Clear();
+
+        foreach (GameObject gameObject in gameObjects)
+        {
+            float distance = Vector3.Distance(transform.position, gameObject.transform.position);
+            if (distance <= minimumAllyDistance && gameObject.GetComponent<ObjectInfo>().currentHealth > 0 && distance > 0)
+            {
+                closeAllies.Add(distance, gameObject);
+            }
+
+        }
+
+        return closeAllies.Count > 0;
     }
 
     private void GoToNextPoint()
@@ -107,9 +190,17 @@ public class RuleBasedAI : MonoBehaviour
             .First()
             .Value;
     
+    public GameObject GetClosestAlly() =>
+             closeAllies
+            .OrderBy(gameObject => gameObject.Key)
+            .ToDictionary
+            (
+            gameObject => gameObject.Key, 
+            gameObject => gameObject.Value
+            )
+            .First()
+            .Value;
+    
 
-    private void Talk()
-    {
-        throw new System.NotImplementedException();
-    }
+
 }
